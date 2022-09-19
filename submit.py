6da477bfc6c6614f3e91aaf5149f96f36815a488
adrota <53432540+adrota@users.noreply.github.com>
2022-09-19 14:11:17 +0200

    Update submit.py

--- submit.py
+++ submit.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 # coding=utf-8
+
+#   submit.py - Generic script for Job submission to Computerome2 HPC (original owner is Nanna Barnkob)
+#   Copyright (C) 2022  Adrian Otamendi Laspiur adrota@dtu.dk
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import time
 import argparse
